9063aa72b79c4fa6c007c063d95c7e4dbbef5f34
Your Name <kir21004@byui.edu>
2024-09-21 14:00:47 -0600

    Tested and Finished the last name first name assignment.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -3,7 +3,18 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
+    {   //Ask and get first name.//
+        Console.WriteLine("Hello! What is your first name?");
+        string firstName = Console.ReadLine();
+
+        //Ask and get last name addresing by first name.//
+        Console.WriteLine($"What is your last name {firstName}?");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine(); //Blank Line for spacing.//
+
+        //Writes back their last name, first name, lastname.//
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+
     }
 }
