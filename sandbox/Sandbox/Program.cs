86de2ef9328bb05d263357add47e5c2fe382dd81
Your Name <kir21004@byui.edu>
2024-09-24 08:35:18 -0600

    Created a Simple Calculator

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Goodbye Sandbox World!");
+        Console.WriteLine("Simple Addition Calculator");
+
+        Console.WriteLine();
+
+        Console.WriteLine("What is your first number?");
+
+        string firstString = Console.ReadLine();
+        int firstNumber = int.Parse(firstString);
+
+        Console.WriteLine("What is your second number?");
+
+        string secondString = Console.ReadLine();
+        int secondNumber = int.Parse(secondString);
+
+        int total = firstNumber + secondNumber;
+
+        Console.WriteLine();
+
+        Console.WriteLine($"The answer for, {firstNumber} + {secondNumber} = {total}");
     }
 }
