d08e94638064ae73632129599f2e484648d73644
Your Name <kir21004@byui.edu>
2024-10-02 17:41:19 -0600

    Made two new files, Resume.cs and Job.cs for our new classes/objects to be used within the learning 02 program.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job joba = new Job();
+        joba._jobTitle = "Software Engineer";
+        joba._company = "Micron";
+        joba._startYear = 2019;
+        joba._endYear = 2022;
+
+        Job jobb = new Job();
+        jobb._jobTitle = "Janitor";
+        jobb._company = "BYU-I";
+        jobb._startYear = 2022;
+        jobb._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "John Allen";
+
+        myResume._jobs.Add(joba);
+        myResume._jobs.Add(jobb);
+
+        myResume.Display();
     }
 }
