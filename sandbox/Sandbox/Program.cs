8f74708ddff5380311fadf0924fa1c0e4e41aba8
Your Name <kir21004@byui.edu>
2024-10-02 17:41:55 -0600

    Added some inclass work/testing.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,24 +5,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Simple Addition Calculator");
+         //We create a new list called numbersList.//
+        List<string> groceryList = new List<string>();
+
+        //Seting while loop to go until user enters 0.//
+        string entry = "";
+        while (entry != "done")
+        {
+            //Asks for a number to be added and adds it to the list unless it is 0.//
+            Console.WriteLine("Please enter your groceries.");
+            entry = Console.ReadLine();
+
+            if (entry != "done")
+            {
+                groceryList.Add(entry);
+            }
+
+        } 
 
         Console.WriteLine();
+        
+        int totalItems = groceryList.Count;
 
-        Console.WriteLine("What is your first number?");
+        Console.WriteLine($"Grocery List ({totalItems} items)");
 
-        string firstString = Console.ReadLine();
-        int firstNumber = int.Parse(firstString);
-
-        Console.WriteLine("What is your second number?");
-
-        string secondString = Console.ReadLine();
-        int secondNumber = int.Parse(secondString);
-
-        int total = firstNumber + secondNumber;
-
+        foreach(string i in groceryList)
+        {
+            Console.WriteLine(i);
+        }
+        
         Console.WriteLine();
 
-        Console.WriteLine($"The answer for, {firstNumber} + {secondNumber} = {total}");
+        //Get user input and print it out//
+        Console.WriteLine("What is your comment?");
+        string commet = Console.ReadLine();
+        Console.WriteLine($"Your comment is: {commet}");
+        Console.WriteLine();
+
+        List<string> animalList = new List<string>();
+        animalList.Add("Goat");
+        animalList.Add("Sheep");
+        animalList.Add("Cow");
+
+        Console.WriteLine("Animals");
+        foreach (string i in animalList)
+        {
+            Console.WriteLine(i);
+        }
+
     }
 }
